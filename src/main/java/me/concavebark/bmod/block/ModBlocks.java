0996a8fb2fcbde647eeb0e3f5ed608beb7e908ba
Concavebark <concavebarkplaysgames@yahoo.com>
2023-03-19 15:20:36 -0400

    REFIXED MODID SORRY JAM IM DUM

---
+++ src/main/java/me/concavebark/bmod/block/ModBlocks.java
@@ -0,0 +1,9 @@
+package me.concavebark.bmod.block;
+
+import net.minecraft.block.Block;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class ModBlocks {
+    //public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, bmod.MOD_ID)
+}
--- src/main/java/me/concavebark/bmod/bmod.java
+++ src/main/java/me/concavebark/bmod/bmod.java
@@ -1,4 +1,4 @@
-package com.conca.bmod;
+package me.concavebark.bmod;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
@@ -19,7 +19,7 @@ import org.apache.logging.log4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("bmod")
+@Mod("bmod2")
 public class bmod
 {
     // Directly reference a log4j logger.
