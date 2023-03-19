5e42b7189ae49e6c93e2549062ba270b465c37ab
Concavebark <concavebarkplaysgames@yahoo.com>
2023-03-19 15:23:32 -0400

    Changed the mod_id in Java files to be dependent on a final String in bmod.java called MOD_ID

--- src/main/java/me/concavebark/bmod/block/ModBlocks.java
+++ src/main/java/me/concavebark/bmod/block/ModBlocks.java
@@ -1,9 +1,10 @@
 package me.concavebark.bmod.block;
 
+import me.concavebark.bmod.bmod;
 import net.minecraft.block.Block;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
 public class ModBlocks {
-    //public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, bmod.MOD_ID)
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, bmod.MOD_ID);
 }
--- src/main/java/me/concavebark/bmod/bmod.java
+++ src/main/java/me/concavebark/bmod/bmod.java
@@ -19,11 +19,12 @@ import org.apache.logging.log4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("bmod2")
+@Mod(bmod.MOD_ID)
 public class bmod
 {
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
+    public static final String MOD_ID = "bmod2";
 
     public bmod() {
         // Register the setup method for modloading
