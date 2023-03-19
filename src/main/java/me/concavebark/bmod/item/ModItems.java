f53cbff81b11f15c03c4b89469951e391e93fc76
whicc <jameskoehler1212@gmail.com>
2023-03-19 17:44:21 -0400

    finished item boilerplate (some missing localization and model)

--- src/main/java/me/concavebark/bmod/item/ModItems.java
+++ src/main/java/me/concavebark/bmod/item/ModItems.java
@@ -12,18 +12,32 @@ public class ModItems {
 
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, bmod.MOD_ID);
 
-    // TODO: Seeds
+    // Seeds
+    // TODO: Localize and make model files
+    public static final RegistryObject<Item> REG_SEED = ITEMS.register("regseeds", () -> new Item(new Item.Properties().tab(ItemGroup.TAB_MISC)));
+    public static final RegistryObject<Item> PERP_SEED = ITEMS.register("perpseeds", () -> new Item(new Item.Properties().tab(ItemGroup.TAB_MISC)));
+    public static final RegistryObject<Item> PRES_SEED = ITEMS.register("presseeds", () -> new Item(new Item.Properties().tab(ItemGroup.TAB_MISC)));
+    public static final RegistryObject<Item> OL_SEED = ITEMS.register("olseeds", () -> new Item(new Item.Properties().tab(ItemGroup.TAB_MISC)));
+    public static final RegistryObject<Item> PK_SEED = ITEMS.register("pkseeds", () -> new Item(new Item.Properties().tab(ItemGroup.TAB_MISC)));
+    public static final RegistryObject<Item> SD_SEED = ITEMS.register("sdseeds", () -> new Item(new Item.Properties().tab(ItemGroup.TAB_MISC)));
 
-    // TODO: Strains
+    // Strains
+    // TODO: Localize and make model files
+    public static final RegistryObject<Item> REG_WEED = ITEMS.register("regweed", () -> new Item(new Item.Properties().tab(ItemGroup.TAB_MISC)));
+    public static final RegistryObject<Item> PERP_WEED = ITEMS.register("perpweed", () -> new Item(new Item.Properties().tab(ItemGroup.TAB_MISC)));
+    public static final RegistryObject<Item> PRES_WEED = ITEMS.register("presweed", () -> new Item(new Item.Properties().tab(ItemGroup.TAB_MISC)));
+    public static final RegistryObject<Item> OL_WEED = ITEMS.register("olweed", () -> new Item(new Item.Properties().tab(ItemGroup.TAB_MISC)));
+    public static final RegistryObject<Item> PK_WEED = ITEMS.register("pkweed", () -> new Item(new Item.Properties().tab(ItemGroup.TAB_MISC)));
+    public static final RegistryObject<Item> SD_WEED = ITEMS.register("sdweed", () -> new Item(new Item.Properties().tab(ItemGroup.TAB_MISC)));
 
     // Blunts
     public static final RegistryObject<Item> ROLLING_PAPERS = ITEMS.register("rpapers", () -> new Item(new Item.Properties().tab(ItemGroup.TAB_MATERIALS)));
-    public static final RegistryObject<Item> BLUNT = ITEMS.register("blunt", () -> new Item(new Item.Properties().tab(ItemGroup.TAB_FOOD)));
+    public static final RegistryObject<Item> REG_BLUNT = ITEMS.register("blunt", () -> new Item(new Item.Properties().tab(ItemGroup.TAB_FOOD)));
     public static final RegistryObject<Item> OOMPA_LOOMPA_BLUNT = ITEMS.register("olblunt", () -> new Item(new Item.Properties().tab(ItemGroup.TAB_FOOD)));
     public static final RegistryObject<Item> PERP_BLUNT = ITEMS.register("perpblunt", () -> new Item(new Item.Properties().tab(ItemGroup.TAB_FOOD)));
     public static final RegistryObject<Item> PRES_BLUNT = ITEMS.register("presblunt", () -> new Item(new Item.Properties().tab(ItemGroup.TAB_FOOD)));
-    public static final RegistryObject<Item> P_K_BLUNT = ITEMS.register("pkblunt", () -> new Item(new Item.Properties().tab(ItemGroup.TAB_FOOD)));
-    public static final RegistryObject<Item> S_D_BLUNT = ITEMS.register("sdblunt", () -> new Item(new Item.Properties().tab(ItemGroup.TAB_FOOD)));
+    public static final RegistryObject<Item> PK_BLUNT = ITEMS.register("pkblunt", () -> new Item(new Item.Properties().tab(ItemGroup.TAB_FOOD)));
+    public static final RegistryObject<Item> SD_BLUNT = ITEMS.register("sdblunt", () -> new Item(new Item.Properties().tab(ItemGroup.TAB_FOOD)));
 
     // Edibles
     public static final RegistryObject<Item> EDIBLE_COOKIE = ITEMS.register("ecookie", () -> new Item(new Item.Properties().tab(ItemGroup.TAB_FOOD)));
